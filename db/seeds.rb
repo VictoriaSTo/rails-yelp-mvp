0bf2e402abf687c76be89be532a08dd2922fd061
Viktoriia Stoichuk <v.stoichuk@gmail.com>
2021-05-29 16:20:41 +0800

    created two-model Rails app with a restaurant and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+tochito = { name: "Tochito", address: "7 Boundary St, London E2 7JE", category: "belgian" }
+teqo =  { name: "Teqo", address: "56A Shoreditch High St, London E1 6PQ", category: "belgian" }
+morzanno =  { name: "Morzanno", address: "42A Shoreditch High St, Shenzhen E1 6PQ", category: "italian" }
+la_marco =  { name: "La Marco", address: "56A Shoreditch High St, Zhuhai E1 6PQ", category: "french"}
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, Beijing E1 6PQ", category: "chinese" }
+
+[tochito, teqo, morzanno, la_marco, pizza_east].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
